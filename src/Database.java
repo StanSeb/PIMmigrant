4a8d2da791844e135da90db05b090019302e3b36
Alianne <alianneelm@yahoo.se>
2020-11-05 10:25:03 +0100

    Connection
    Connection to DB

--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+
+    private Connection conn;
+
+    public Database(){
+
+        try{
+            conn = DriverManager.getConnection("jdbc:sqlite:PIMmigrant_DB.db");
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+    }
+
 
 
 }
