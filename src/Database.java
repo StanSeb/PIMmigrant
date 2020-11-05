6d6b536accca81aa3d58db6648586d8b63abe522
Alianne <alianneelm@yahoo.se>
2020-11-05 11:31:57 +0100

    Function CreateNotes();
    Function to create new notes and add them to the database

--- src/Database.java
+++ src/Database.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Database {
@@ -10,6 +11,18 @@ public class Database {
 
         try{
             conn = DriverManager.getConnection("jdbc:sqlite:PIMmigrant_DB.db");
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+    }
+
+    public void createNotes(Note notes){
+        try{
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO notes (content) VALUES (?)");
+            stmt.setString(1, notes.getContent());
+
+            stmt.executeUpdate();
+
         }catch (SQLException throwables){
             throwables.printStackTrace();
         }
