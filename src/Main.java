bdd0e300caa0466f66f7711949f5e171314e022b
Sebastian Stanczak <stanczak.sebastian@gmail.com>
2020-11-05 09:39:35 +0100

    Third commit
    Added all the files

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import express.Express;
+import express.middleware.Middleware;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Express app = new Express();
+
+        try{
+            app.use(Middleware.statics(Paths.get("src/www").toString()));
+        }catch(IOException e){e.printStackTrace();}
+
+
+        app.listen(1000);
+        System.out.println("System started at port 1000");
+
+    }
+}
