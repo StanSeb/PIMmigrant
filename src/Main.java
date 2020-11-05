cffdb0089e8480fb3e4bd0142f738dd88fb3c9f9
FCO <franne_@hotmail.com>
2020-11-05 11:38:18 +0100

    Express - La till app.get()

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,22 @@ import express.middleware.Middleware;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Express app = new Express();
+        app.get("/",(req, res)->{
+            res.send();
+        });
+
+        app.get("/rest/Note", (req, res)->{
+            List<Note> notes = db.getNotes();
+
+            res.json(notes);
+        });
 
         try{
             app.use(Middleware.statics(Paths.get("src/www").toString()));
