bcd88893f0c5575ed74836eff314faec138afc58
Sebastian Stanczak <stanczak.sebastian@gmail.com>
2020-11-06 10:20:38 +0100

    Added some JS

---
+++ src/www/script.js
@@ -0,0 +1,107 @@
+async function getNotes() {
+    let result = await fetch("/rest/Notes");
+    notesArray = await result.json();
+
+    renderNotes();
+    assignDeleteButtons();
+    checkNote();
+}
+
+function renderNotes() {
+    var noteList = document.querySelector("#notes-list");
+
+    noteList.innerHTML = "";
+
+    for (let note of notesArray) {
+        let noteLi = `<li class="note-li">${note.note}</li>`;
+
+        noteList.innerHTML += noteLi;
+    }
+
+    let allLi = document.getElementsByTagName("li");
+    let i;
+
+    for (i = 0; i < allLi.length; i++) {
+
+        const divButtons = document.createElement("DIV");
+        const addButton = document.createElement("button");
+        const saveButton = document.createElement("button");
+        const deleteButton = document.createElement("button");
+
+        addButton.className = "add-btn";
+        saveButton.className = "save-btn";
+        deleteButton.className = "delete-btn";
+        divButtons.className = "div-buttons";
+
+        addButton.innerHTML = '<i class="fas fa-plus"></i>'
+        saveButton.innerHTML = '<i class="fas fa-check"></i>';
+        deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+
+        divButtons.appendChild(addButton);
+        divButtons.appendChild(saveButton);
+        divButtons.appendChild(deleteButton);
+
+        allLi[i].appendChild(divButtons);
+
+    }
+
+}
+
+function assignDeleteButtons() {
+    listOfButtons = document.getElementsByClassName("delete-btn");
+
+    var parentToDelete = document.getElementsByClassName("div-buttons");
+
+
+    for (let index = 0; index < listOfButtons.length; index++) {
+        listOfButtons[index].onclick = function () {
+            parentToDelete[index].parentElement.style.display = "none";
+            deleteNote(index);
+        }
+
+    }
+}
+
+
+async function addNote() {
+
+    let titleField = document.getElementById("title");
+    let inputField = document.getElementById("note");
+
+    let inputFieldValue = inputField.value;
+    let titleFieldValue = titleField.value;
+
+    let theTitleBody = JSON.stringify({title: titleFieldValue });
+    let theInputBody = JSON.stringify({note: inputFieldValue})
+
+    if (inputFieldValue == "" && titleFieldValue == "") {
+        alert("This field can not be empty");
+    } else {
+
+        let result = await fetch("/rest/Notes", {
+            method: "POST",
+            body: theInputBody
+        });
+
+        console.log(await result.text());
+
+        renderNotes();
+    }
+}
+
+async function deleteNote(index) {
+
+    let noteToDelete = notesArray[index].id;
+
+    let theBody = {
+        id: noteToDelete
+    }
+
+    console.log(noteToDelete);
+    let result = await fetch("/rest/Notes/id", {
+        method: "DELETE",
+        body: JSON.stringify(theBody)
+    });
+
+    renderNotes();
+}
