3d77abb2005bc3d95bc3955d6a1cb4a5f61f51e2
FCO <franne_@hotmail.com>
2020-11-12 09:34:10 +0100

    Alex´s fel

--- src/Note.java
+++ src/Note.java
@@ -1,38 +1,23 @@
+import java.util.Date;
+
 public class Note {
 
     private int id;
     private String title;
     private String content;
-    private String filename;
-    private int note_id;
-    private int timestamp;
+    private long timestamp;
+    private String imgUrl;
 
-
-    public Note(int id, String title, String content, int timestamp, String filename, int note_id) {
+    public Note(int id, String title, String content, long timestamp, String imgUrl) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.timestamp = timestamp;
-        this.filename = filename;
-        this.note_id = note_id;
+        this.imgUrl = imgUrl;
     }
+
     public Note() {}
 
-    public int getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(int timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
 
     public int getId() {
         return id;
@@ -58,13 +43,22 @@ public class Note {
         this.content = content;
     }
 
-    public int getNote_id() {
-        return note_id;
+    public long getTimestamp() {
+        return timestamp;
     }
 
-    public void setNote_id(int note_id) {
-        this.note_id = note_id;
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
     }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
 
     @Override
     public String toString() {
@@ -72,11 +66,8 @@ public class Note {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
-                ", filename='" + filename + '\'' +
-                ", note_id=" + note_id +
                 ", timestamp=" + timestamp +
+                ", imgUrl='" + imgUrl + '\'' +
                 '}';
     }
 }
-
-
